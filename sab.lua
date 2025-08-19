07cf2e196655016953d5b1f109921ed24acf623d
Bant3r241 <bobby36ko@outlook.com>
2025-08-19 05:24:44 -0400

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -1,8 +1,44 @@
 if game.PlaceId == 109983668079237 then
     local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
-    local Window = OrionLib:MakeWindow({Name="ABI │ Steal A Brainrot", HidePremium=false, IntroEnabled=false, IntroText="ABI", SaveConfig=true, ConfigFolder="XlurConfig"})
+    local Window = OrionLib:MakeWindow({Name="ABI │ Steal A Brainrot",HidePremium=false,IntroEnabled=false,IntroText="ABI",SaveConfig=true,ConfigFolder="XlurConfig"})
 
- local MainTab = Window:MakeTab({Name = "Main"})
+   
 
+
+
+    local MainTab = Window:MakeTab({Name="Main",Icon="rbxassetid://4299432428",PremiumOnly=false})
+   
+
+    local EspTab=Window:MakeTab({Name="ESP",Icon="rbxassetid://4299432428",PremiumOnly=false})
+
+  
+    local MiscTab=Window:MakeTab({Name="Misc",Icon="rbxassetid://4299432428",PremiumOnly=false})
+  
+
+    MiscTab:AddSlider({
+        Name="Walk Speed",
+        Min=16,
+        Max=100,
+        Default=16,
+        Increment=1,
+        Color=Color3.new(1,1,1),
+        ValueName="Speed",
+        Callback=function(val)
+            _G.WalkSpeed=val
+            local plr=game.Players.LocalPlayer
+            local char=plr.Character or plr.CharacterAdded:Wait()
+            local hum=char:WaitForChild("Humanoid")
+            hum.WalkSpeed=val
+        end
+    })
+
+    MiscTab:AddToggle({Name="ESP",Default=false,Callback=function(v)
+        _G.ESP=v
+        if v then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Bant3r241/chams/refs/heads/main/ESP.lua"))()
+        end
+    end})
+
+end
 
 OrionLib:Init()
